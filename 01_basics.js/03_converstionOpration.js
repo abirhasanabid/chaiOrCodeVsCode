99d9c72667ccbc7f246be67120cf9821410ea0be
abid <abid.7.tm@gmail.com>
2025-06-06 22:27:17 +0600

    convertion Opration class 6 compleated

--- 01_basics.js/03_converstionOpration.js
+++ 01_basics.js/03_converstionOpration.js
@@ -1,5 +1,38 @@
 let score = '20';
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+
+const stringOfNumbers = '121343';
+// console.log(typeof (stringOfNumbers));
+// console.log(typeof Number((stringOfNumbers)));
+
+const stringOfNumbers2 = '12dfjdf';
+// console.log(Number(stringOfNumbers2));
+
+const boolean = false;
+// console.log(Number(boolean));
+
+
+// '11' => 11
+// "11fg" => NaN
+// true => 1 : false => 0
+
+const boolNumber  = 'DF';
+// console.log(Boolean(boolNumber));
+
+/* NOTES */
+// 1=> TRUE
+// 0=> FALSE
+// ''=> FALSE
+// 'ABOID'=> TRUE
+
+const justNumber = 12;                            
+console.log(typeof justNumber);                   /* just number */
+
+const convertToStr = String(justNumber);
+console.log(typeof convertToStr);    /* convert something to string */
+
