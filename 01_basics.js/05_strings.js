55d75a5bcead5dd89a8e1dea54ee5f84150afc30
abid <abid.7.tm@gmail.com>
2025-06-18 06:50:16 +0600

    just 2 of string method

---
+++ 01_basics.js/05_strings.js
@@ -0,0 +1,18 @@
+const name = 'Abid hasan Abir';
+const age = 20;
+
+// console.log(name + age); /* bad way to concat variable in js */
+
+console.log(`My name is ${name.toUpperCase()} and my age is ${age}`);  /* good way to concat variable in js */
+
+
+const newStr = new String('Just a string');
+console.log(newStr);
+console.log(newStr.length);
+console.log(newStr.charAt(3));  
+console.log(newStr.indexOf('g'));
+
+
+
+
+
