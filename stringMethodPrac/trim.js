3deba3cf3ccd8cf43afa60343cce4f82b738646b
abid <abid.7.tm@gmail.com>
2025-08-25 23:21:42 +0600

    trim example in login scnirion

--- stringMethodPrac/trim.js
+++ stringMethodPrac/trim.js
@@ -1,5 +1,5 @@
 const str = " hello world ";
-console.log(str.trim());
+// console.log(str.trim());
 
 
 /* Check if Input is Empty
@@ -8,32 +8,47 @@ User enters spaces — check if it's empty after trimming. */
 
 const userInput = '';
 if (userInput.trim() === '') {
-    console.log('Input is empty');
+    // console.log('Input is empty');
 } else {
-    console.log('Input is valid');
+    // console.log('Input is valid');
 };
 
 /* Count Real Characters
 🧩 Problem:
-Get the number of real characters after trimming.
+Get the number of real characters after trimming.   
  */
 
 const text = "   hello world   ";
 const trimValue = text.trim();
-console.log(trimValue.length);
+// console.log(trimValue.length);
 
 /* Compare Cleaned Strings
 🧩 Problem:
 Check if a username matches "admin" after trimming. */
 
-const  adminStr = "  ad min   ";
+const adminStr = "  ad min   ";
 
 // console.log(adminStr.trim() .replace(' ',''));
 
-if (adminStr.trim().replace(' ','').toLowerCase()==="admin") {
-    console.log('Access Granted');
+if (adminStr.trim().replace(' ', '').toLowerCase() === "admin") {
+    // console.log('Access Granted');
 } else {
-    console.log('Access Denied');
+    // console.log('Access Denied');
+
+}
+
+let text4 = ' hello world';
+console.log(text4);
+const text4Result = text4.trim();
+console.log(text4Result);
+
+// Trim example in login senerio ;
+let emailInput = "   user@example.com   ";
+const finalEnput = emailInput.trim();
+if (finalEnput === "user@example.com") {
+    console.log("Access Granted 👉");
+}else{
+    console.log("Access Invalid");
     
 }
 
