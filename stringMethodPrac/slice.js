bba96cbd334d5cb19bdac715af1587b8bf2e0fdb
abid <abid.7.tm@gmail.com>
2025-08-30 22:25:40 +0600

    just some string slice metod prcatice

---
+++ stringMethodPrac/slice.js
@@ -0,0 +1,12 @@
+let text = "JavaScript";
+const justJava = text.slice(0,4);
+const justJavaNeg = text.slice(-6)
+
+console.log(justJava);
+console.log(justJavaNeg);
+
+// 1. Fornt
+// 2.Script
+// 3.Bang
+// 4.actJS
+// 5. loper
