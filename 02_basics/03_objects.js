462ff4999538b8c1ede3fb4568ea75d56527b812
abid <abid.7.tm@gmail.com>
2025-09-11 06:17:26 +0600

    just a single example of object destructring

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -34,9 +34,20 @@ const objValues = Object.values(user);
 console.log(objValues);
 
 // meage 2 objects
-const obj1 = {1:'a',2:'b'};
-const obj2 = {3:'a',4:'b'};
+const obj1 = { 1: 'a', 2: 'b' };
+const obj2 = { 3: 'a', 4: 'b' };
 
-const meargeObj = {...obj1,...obj2};
+const meargeObj = { ...obj1, ...obj2 };
 console.log(meargeObj);
 
+// 
+// Q1: Use destructuring to extract name and city
+// Q2: Rename "age" into "userAge" while destructuring
+
+const identity = {
+  name: "Rahim",
+  age: 22,
+  city: "Dhaka"
+};
+let { name, city} = identity;
+console.log(city);
