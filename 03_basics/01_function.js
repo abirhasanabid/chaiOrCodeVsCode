9b7d77246884ba3432e41dd746654d5e5b2a2b3a
abid <abid.7.tm@gmail.com>
2025-09-12 23:15:27 +0600

    just some function thing

---
+++ 03_basics/01_function.js
@@ -0,0 +1,22 @@
+function sumOFTheNumbes(num1,num2) {
+    return num1 + num2
+    
+};
+const result =  sumOFTheNumbes(2,4);
+console.log(result);
+
+
+// Function without parameters
+
+function sayHello(){
+console.log("hello");
+
+}
+sayHello();
+
+// Write a function named sayMyName that prints your name when you call it.
+function sayMyName(name){
+console.log(name);
+
+}
+sayMyName("abir")
