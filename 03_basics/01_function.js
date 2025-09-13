2d729b86b90c23873865b4260568f64561f83680
abid <abid.7.tm@gmail.com>
2025-09-14 05:30:53 +0600

    some basic function practice

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -49,10 +49,64 @@ function biggestNumber(a, b) {
     if (a > b) {
         return `a is the bigger than b`;
     }
-    else{
+    else {
         return `b is smaller than a`;
     }
 
 }
-const biggestNumberResult = biggestNumber(2,5);
+const biggestNumberResult = biggestNumber(2, 5);
 console.log(biggestNumberResult);
+
+// Write a function called findSquare that takes 1 number and returns its square.
+
+function findSquare(num) {
+    return num * num;
+};
+const squeResult = findSquare(2);
+console.log(squeResult);
+
+
+// Find the bigger number
+function findBigger(num1 = 0, num2 = 0) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+};
+const resultOfTheBiggerNumber = findBigger(10, 20);
+const resultOfTheBiggerNumber1 = findBigger(50, 30);
+console.log(resultOfTheBiggerNumber);
+console.log(resultOfTheBiggerNumber1);
+
+
+
+// Add 3 Numbers
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+};
+console.log(addThreeNumbers(2, 3, 4));
+console.log(addThreeNumbers(10, 20, 30));
+
+// Greeting Function
+function greetUser(name) {
+    return `Hello ${name}, welcome!`
+};
+console.log(greetUser('Abir'));
+console.log(greetUser('Rahim'));
+
+// Number Greater Than 10?
+function isGreaterThanTen(n) {
+    if (n > 10) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+    // return n > 10;     /* this is the shorter version of 100 to 105 number line */
+};
+console.log(isGreaterThanTen(10));
+console.log(isGreaterThanTen(7));
+console.log(isGreaterThanTen(15));
+
