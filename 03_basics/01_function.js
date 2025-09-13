b17ef07ebf9391e100b3667cef0f83970c1f3209
abid <abid.7.tm@gmail.com>
2025-09-13 22:58:04 +0600

    just the starting of function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,22 +1,58 @@
-function sumOFTheNumbes(num1,num2) {
+function sumOFTheNumbes(num1, num2) {
     return num1 + num2
-    
+
 };
-const result =  sumOFTheNumbes(2,4);
+const result = sumOFTheNumbes(2, 4);
 console.log(result);
 
 
 // Function without parameters
 
-function sayHello(){
-console.log("hello");
+function sayHello() {
+    console.log("hello");
 
 }
 sayHello();
 
 // Write a function named sayMyName that prints your name when you call it.
-function sayMyName(name){
-console.log(name);
+function sayMyName(name) {
+    console.log(name);
 
 }
-sayMyName("abir")
+sayMyName("abir");
+
+
+// write a function named multiply that takes two numbers and prints their multiplication result.
+
+function multiply(a, b) {
+    return a * b;
+}
+const resultOfMulktiplay = multiply(4, 3);
+console.log(resultOfMulktiplay);
+
+// Even or Odd checker
+function isEven(number) {
+    if (number % 2 === 0) {
+        return `This is an even number`
+    }
+    else {
+        return `odd number`
+    }
+};
+
+const checkNumber = isEven(2);
+console.log(checkNumber);
+
+
+// Biggest of two numbers
+function biggestNumber(a, b) {
+    if (a > b) {
+        return `a is the bigger than b`;
+    }
+    else{
+        return `b is smaller than a`;
+    }
+
+}
+const biggestNumberResult = biggestNumber(2,5);
+console.log(biggestNumberResult);
