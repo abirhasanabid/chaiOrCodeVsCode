dc21bbe51922c46e19f253e4a57d51abad1e715e
abid <abid.7.tm@gmail.com>
2025-09-23 00:38:30 +0600

    some grade and discount probleam i control flow

--- 04_basics/01_controlFlow.js
+++ 04_basics/01_controlFlow.js
@@ -14,7 +14,7 @@ const findNegetive = function (num) {
     if (num > 0) {
         return `The number ${num} is positve`;
 
-    } else if(num === 0){
+    } else if (num === 0) {
         return `The number is ${num}`;
     }
     else {
@@ -26,4 +26,70 @@ console.log(findNegetive(20));
 console.log(findNegetive(-20));
 console.log(findNegetive(0));
 console.log(findNegetive(-5));
-console.log( findNegetive(10));
+console.log(findNegetive(10));
+
+
+// Check if a person is adult (age ≥ 18) or minor.
+
+const age = 10;
+
+if (age >= 18) {
+    console.log(`The person is an adult`);
+
+} else {
+    console.log(`Minor`);
+
+};
+
+
+// Write a program that gives grades:
+
+// Marks ≥ 80 → A+
+
+// Marks ≥ 60 → A
+
+// Marks ≥ 40 → B
+
+// Else → Fail
+
+const marks = 80;
+if (marks >= 80) {
+    console.log(`A+`);
+
+} else if (marks >= 60) {
+    console.log(`A`);
+} else if (marks >= 40) {
+    console.log(`B`);
+
+} else {
+    console.log(`Better luck next time`);
+};
+
+
+// A shop gives discounts:
+
+// If price ≥ 500 → 20% discount
+
+// If price ≥ 200 → 10% discount
+
+// Else → no discount
+
+const price = 300;
+
+if (price >= 500) {
+    const discount = 20 / 100;
+    const discountAmount = price * discount;
+    const discountPrice = price - discountAmount;
+    console.log(discountPrice);
+
+
+} else if (price >= 200) {
+    const discount = 10 / 100;
+    const discountAmount = price * discount;
+    const discountPrice = price - discountAmount;
+    console.log(discountPrice);
+
+} else {
+    console.log(`There is no discount`);
+
+};
