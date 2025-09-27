1f8729988453cb7a85cb93cfd60280dfa62fb1a1
abid <abid.7.tm@gmail.com>
2025-09-27 23:57:19 +0600

    just switch start

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 0;
+
+switch (month) {
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`February`);
+        break;
+    case 3:
+        console.log(`March`);
+        break;
+    case 4:
+        console.log(`Afril`);
+        break;
+    case 5:
+        console.log(`May`);
+        break;
+    case 6:
+        console.log(`june`);
+        break;
+    case 7:
+        console.log(`july`);
+        break;
+    case 8:
+        console.log(`August`);
+        break;
+    case 9:
+        console.log(`september`);
+        break;
+    case 10:
+        console.log(`october`);
+        break;
+    case 11:
+        console.log(`novembor`);
+        break;
+    case 12:
+        console.log(`december`);
+        break;
+
+    default:
+        console.log(`There is no month in this number`);
+        break;
+};
