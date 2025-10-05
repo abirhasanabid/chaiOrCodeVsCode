614f87bc7b2f156bb72ee9c24d889df50b430844
abid <abid.7.tm@gmail.com>
2025-10-05 07:21:23 +0600

    ternary oprator exercise

---
+++ 04_controlFlow/ternary.js
@@ -0,0 +1,18 @@
+// Check if a number is positive or negative using a ternary operator.
+const number = -120;
+const positive = number < 0 ? 'Negative' : 'positive';
+console.log(positive);
+
+
+// Check if a person is eligible to vote (age 18 or more).
+
+const age = 12;
+const forVote = age >= 18 ? "eligible" : "not eligible";
+console.log(forVote);
+
+
+// If a user is logged in, show "Dashboard"; otherwise, show "Login Page".
+
+const isLoggedIn = true;
+const login = isLoggedIn ? "Dashbord" : "Login Page";
+console.log(login);
