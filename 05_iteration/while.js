25b04af83d8b51d99777f9cc6d978b4504eb9461
abid <abid.7.tm@gmail.com>
2025-10-09 22:45:37 +0600

    while loop practice and compleated the steap one of chatgpt

---
+++ 05_iteration/while.js
@@ -0,0 +1,38 @@
+let i = 1;
+while (i <= 10) {
+    console.log(`${i}`);
+    i++;
+};
+
+
+// Print even numbers from 2 to 10.
+let even = 2;
+while (even <= 10) {
+    console.log(even);
+    even += 2;
+};
+
+// Count down from 10 to 1.
+let decrement = 10;
+while (decrement >= 1) {
+    console.log(decrement);
+    decrement--;
+};
+
+
+// Find the sum of numbers from 1 to 10.
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+console.log(sum);
+
+
+let str = 'hello';
+let i2 = 0;
+while (i2 < str.length) {
+    console.log(str[i2]);
+    i2++;
+};
+
